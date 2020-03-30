6286c50c79f6150fe27d034c04c03dfc1d85eed2
flsanche <florian.sanchez@mail.concordia.ca>
2020-03-30 11:17:46 -0400

    Initial development
    Added air properties functions
    Added operating conditions object that contains T_ISA, T_stat, T_tot, Pressure functions

---
+++ airProperties.py
@@ -0,0 +1,50 @@
+# Air properties
+# This object defines the classes and functions used to calculate the air properties
+
+import math
+import operatingConditions as opCond
+
+class airProperties():
+    '''
+    # Calculation of the air properties for specific operating conditions
+    operatingConditions: operating conditions of the aircraft
+    zoneTemperature: average temperature of the zone under study
+    '''
+    # Sea level conditions
+    pressureSL = 101325.
+    densitySL = 1.225
+    temperatureSL = 15.
+    g = 9.80665
+    gamma = 1.4
+    R = 287.04
+
+    def __init__(self,
+                 operatingConditions: object,
+                 zoneTemperature: float,
+                 *args,
+                 **kwargs):
+
+        self.operatingConditions = operatingConditions
+        self.zoneTemperature = zoneTemperature
+
+    def rho_air(self):
+        # Air density in kg/m³
+        Pamb = opCond.operatingConditions.pressure(self.operatingConditions)
+        return Pamb / (airProperties.R * self.zoneTemperature)
+    
+    def k_air(self):
+        # Thermal conductivity of air in W/m/K
+        return 8.E-05 * self.zoneTemperature + 0.0236
+
+    def mu_air(self):
+        # Dynamic viscosity of air in kg/m/s
+        return 5.E-08 * self.zoneTemperature + 2.E-05
+    
+    def beta_air(self):
+        # Thermal expansion coefficient of air in 1/K
+        # Ideal gas assumption
+        return 1. / self.zoneTemperature
+    
+    def Cp_air(self):
+        # Heat capacity of air in J/kg/K
+        return 1.E-05 * self.zoneTemperature**3 - 0.0013 * self.zoneTemperature**2 + 0.0422 * self.zoneTemperature + 1006.4
---
+++ operatingConditions.py
@@ -0,0 +1,123 @@
+# Operating Conditions
+# This object defines the classes and functions used to define the oprating conditions
+
+import math
+import numpy as np
+
+class operatingConditions():
+    '''
+    # Definition of the operating conditions
+    name: name of the operating conditions
+    dayType: type of the day
+    altitude: altitude of the aircraft in ft
+    speed: aircraft speed in Ma
+    '''
+    # Sea level conditions
+    pressureSL = 101325.
+    densitySL = 1.225
+    temperatureSL = 15.
+    g = 9.80665
+    gamma = 1.4
+    R = 287.04
+
+    def __init__(self,
+                 name: str,
+                 dayType: str,
+                 altitude: float,
+                 speed: float,
+                 *args,
+                 **kwargs):
+
+        self.name = name
+        self.dayType = dayType
+        self.altitude = altitude
+        self.speed = speed
+
+    def pressure(self):
+        # Ambient pressure in Pa
+        altitudeMeter = self.altitude / 3.28084
+        T_ISA_K = operatingConditions.T_ISA(self) + 273.15
+        return operatingConditions.pressureSL * (1.-(0.0065*altitudeMeter/T_ISA_K))**5.255
+
+    def T_ISA(self):
+        # International Standard Atmosphere Temperature
+        if self.altitude<=36000.:
+            return operatingConditions.temperatureSL - 1.98 * self.altitude / 1000.
+        else:
+            return -56.5
+    
+    def Tstat(self):
+        # Ambient Static Temperature based on a temperature envelope
+        # Flight conditions
+        if self.speed != 0.:
+            if self.dayType == 'Extra-Hot':
+                if self.altitude <= 15000.:
+                    return operatingConditions.T_ISA + 40.
+                else:
+                    if self.altitude <=35000.:
+                        return operatingConditions.T_ISA + 35.
+                    else:
+                        return operatingConditions.T_ISA + 30.
+            else:
+                if self.dayType == 'Hot':
+                    if self.altitude <= 15000.:
+                        return operatingConditions.T_ISA + 20.
+                    else:
+                        if self.altitude <=35000.:
+                            return operatingConditions.T_ISA + 15.
+                        else:
+                            return operatingConditions.T_ISA + 10.
+                else:
+                    if self.dayType == 'Normal':
+                        return operatingConditions.T_ISA
+                    else:
+                        if self.dayType == 'Cold':
+                            if self.altitude <= 15000.:
+                                return operatingConditions.T_ISA - 30.
+                            else:
+                                if self.altitude <=35000.:
+                                    return operatingConditions.T_ISA - 10.
+                                else:
+                                    return operatingConditions.T_ISA - 15.
+                        else:
+                            if self.dayType == 'Extra-Cold':
+                                if self.altitude <= 15000.:
+                                    return -55.
+                                else:
+                                    if self.altitude <= 30000.:
+                                        return operatingConditions.T_ISA - 30.
+                                    else:
+                                        return operatingConditions.T_ISA - 20.
+        # Ground conditions
+        else:
+            if self.dayType == 'Extra-Hot':
+                if self.altitude < 15000.:
+                    return operatingConditions.T_ISA + 40.
+                else:
+                    return operatingConditions.T_ISA + 45.
+            else:
+                if self.dayType == 'Hot':
+                    if self.altitude < 15000.:
+                        return operatingConditions.T_ISA + 30.
+                    else:
+                        return operatingConditions.T_ISA + 25.
+                else:
+                    if self.dayType == 'Normal':
+                        return operatingConditions.T_ISA
+                    else:
+                        if self.dayType == 'Cold':
+                            if self.altitude < 15000.:
+                                return operatingConditions.T_ISA - 30.
+                            else:
+                                return operatingConditions.T_ISA - 5.
+
+                        else:
+                            if self.dayType == 'Extra-Cold':
+                                if self.altitude <= 10000.:
+                                    return -55.
+                                else:
+                                    return self.altitude * 0.005 - 105
+
+    def Ttot(self):
+        # Total air temperature
+        return (1+((operatingConditions.gamma-1)/2) * self.speed**2) * (operatingConditions.Tstat+273.15) - 273.15
